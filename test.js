11c0495bc15ff19890673ca7b27a3d990dbcf369
Amylo <amylose1110@gmail.com>
2023-03-17 21:11:59 +0900

    initial commit

---
+++ test.js
@@ -0,0 +1,7 @@
+"use strict";
+let a;
+a = 39;
+function hello(b) {
+}
+hello(39);
+hello("Mark");
---
+++ test.ts
@@ -0,0 +1,10 @@
+let a: number
+
+a = 39;
+
+function hello(b: number) {
+
+}
+
+hello(39);
+hello("Mark");
